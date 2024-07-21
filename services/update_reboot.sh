51c834ab694e05d10fe7e45330a62390fd18d4a9
M100ika <maxat.suieubayev@gmail.com>
2024-07-21 19:21:45 +0500

    update

---
+++ services/update_reboot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Git pull и перезагрука
+# Перейти в нужную директорию
+cd /home/pi/scales7.1/scales_submodule/
+
+# Выполнить git pull
+git pull
+
+# Перезагрузить систему
+sudo reboot
+
+
+#Чтобы установить сделайте следующее 
+#crontab -e
+#Добавьте строку в cron 
+#0 4 * * * /home/pi/scales7.1/scales_submodule/services/update_and_reboot.sh >> /home/pi/scales7.1/scales_submodule/services/update_and_reboot.log 2>&1
