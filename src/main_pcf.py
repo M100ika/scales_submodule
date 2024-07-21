1ee1e57bd6908a11fc989e178c38b86896dc52d2
M100ika <maxat.suieubayev@gmail.com>
2024-07-21 16:51:37 +0500

    logging

--- src/main_pcf.py
+++ src/main_pcf.py
@@ -22,8 +22,13 @@ from _glb_val import DEBUG
 config_manager = ConfigManager()
 debug_level = "DEBUG" if DEBUG == 1 else "CRITICAL"
 
-logger.add('log/scales_{time}.log', format="{time} {level} {message}", 
-level="DEBUG", rotation="1 day", compression="zip")             # Настройка логгера
+"""Инициализация logger для хранения записи о всех действиях программы"""
+logger.add('scales_log/scales.log', format="{time} {level} {message}", 
+level=debug_level, rotation="1 day", retention= '1 month', compression="zip")  
+
+"""Инициализация logger для хранения записи об ошибках программы"""
+logger.add('scales_log/error_log/errors.log', format="{time} {level} {file}:{line} {message}", 
+level="ERROR", rotation="1 day", retention= '1 month', compression="zip")          # Настройка логгера
 
 @logger.catch()         # Показывает ошибки, не работает если их обрабатывать
 def main():
