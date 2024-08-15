214f41704fea91e761d2e169994b3989f32e465d
M100ika <maxat.suieubayev@gmail.com>
2024-08-15 14:37:11 +0500

    main

--- src/main_pcf.py
+++ src/main_pcf.py
@@ -27,7 +27,7 @@ logger.add('scales_log/scales.log', format="{time} {level} {message}",
 level=debug_level, rotation="1 day", retention= '1 month', compression="zip")  
 
 """Инициализация logger для хранения записи об ошибках программы"""
-logger.add('scales_log/error_log/errors.log', format="{time} {level} {file}:{line} {message}", 
+logger.add('scales_log/error_log/errors.log', format="{time} {level} {file} {module}:{line} {message}", 
 level="ERROR", rotation="1 day", retention= '1 month', compression="zip")          # Настройка логгера
 
 @logger.catch()         # Показывает ошибки, не работает если их обрабатывать
