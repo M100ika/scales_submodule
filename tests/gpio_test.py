30363349513ec0d55c84827a683df790ba9741d0
M100ika <maxat.suiuebayev@gmail.com>
2025-05-22 13:04:32 +0500

    11==x

--- tests/gpio_test.py
+++ tests/gpio_test.py
@@ -9,10 +9,12 @@ sys.path.append(str(Path(__file__).resolve().parent.parent / 'src'))
 
 from _config_manager import ConfigManager
 
-
+ON_TIME = 1
+OFF_TIME = 1
 config_manager = ConfigManager()
 
 medicine_pin = int(config_manager.get_setting("Sprayer", "medicine_pin"))
+paint_pin = int(config_manager.get_setting("Sprayer", "paint_pin"))
 
 GPIO.setmode(GPIO.BCM)  # Устанавливаем BCM-нумерацию пинов
 GPIO.setup(medicine_pin, GPIO.OUT)  # Устанавливаем 18-й пин как выход
@@ -20,21 +22,23 @@ GPIO.output(medicine_pin, GPIO.LOW)
 
 def main():
     try:
-        print("Enter On time: ")
-        on_time = input()
-        print("Enter off time: ")
-        off_time = input()
+        print("To Medicine pin: ", medicine_pin)
+        print("To Paint pin: ", paint_pin)
         count = 0
         while(True):
             print("On")
             GPIO.output(medicine_pin, GPIO.HIGH)  # Подаём 3.3 вольта на 18-й пин
+            GPIO.output(paint_pin, GPIO.HIGH)  # Подаём 3.3 вольта на 18-й пин
             #GPIO.output(23, GPIO.HIGH)
-            time.sleep(float(on_time))  # Ждём одну секунду
+            time.sleep(float(ON_TIME))  # Ждём одну секунду
             count += 1
             print("off")
             GPIO.output(medicine_pin, GPIO.LOW)  # Подаём 3.3 вольта на 18-й пин
+            GPIO.output(paint_pin, GPIO.LOW)  # Подаём 3.3 вольта на 18-й пин
+
             #GPIO.output(23, GPIO.LOW)
-            time.sleep(float(off_time))
+            time.sleep(float(OFF_TIME))
+            
     except KeyboardInterrupt as e:
         print("Ok! Bye!")
         print("Count of cycles: ", count)
