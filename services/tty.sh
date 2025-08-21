52c336607079901fac050df87395b584eab59ac6
M100ika <maxat.suieubayev@gmail.com>
2025-08-21 18:34:13 +0500

    tty_upd

--- services/tty.sh
+++ services/tty.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-echo 'SUBSYSTEM=="tty", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="7523", SYMLINK+="ttyCH340"' | sudo tee /etc/udev/rules.d/99-usb-serial.rules
-echo 'SUBSYSTEM=="tty", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", SYMLINK+="ttyCP210x"' | sudo tee -a /etc/udev/rules.d/99-usb-serial.rules
+RULES=/etc/udev/rules.d/99-usb-serial.rules
+sudo touch "$RULES"
+
+L1='SUBSYSTEM=="tty", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="7523", SYMLINK+="ttyCH340"'
+L2='SUBSYSTEM=="tty", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", SYMLINK+="ttyCP210x"'
+L3='SUBSYSTEM=="tty", ATTRS{idVendor}=="2341", ATTRS{idProduct}=="0043", SYMLINK+="ttyArduinoUno"'
+
+grep -qF "$L1" "$RULES" || echo "$L1" | sudo tee -a "$RULES" >/dev/null
+grep -qF "$L2" "$RULES" || echo "$L2" | sudo tee -a "$RULES" >/dev/null
+grep -qF "$L3" "$RULES" || echo "$L3" | sudo tee -a "$RULES" >/dev/null
+
 sudo udevadm control --reload-rules
 sudo udevadm trigger
